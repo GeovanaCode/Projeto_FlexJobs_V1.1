5dc0319fcdeadec4a4785717544793dc15ad7675
yLeonardo99 <leo205283@gmail.com>
2024-04-11 21:12:08 -0300

    Atualização
    Co-Authored-By: Henrique <119366049+henriqueandras1@users.noreply.github.com>

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -4,24 +4,24 @@ function formatarCPF(campo) {
     // Remover caracteres indesejados ou seja Letras
 
     let cpf = campo.value.replace(/\D/g, '');
-    
+
     // Adicionando  a formatação
 
     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
     cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-    
-    // Faz com que Atualiza o valor do campo
+
+    //  Atualiza o valor do campo
     campo.value = cpf;
 }
 
 // Requisição da Senha
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let senhaInput = document.getElementById('senha');
     let erroSenha = document.getElementById('erro-senha');
 
-    senhaInput.addEventListener('input', function() {
+    senhaInput.addEventListener('input', function () {
         let senha = this.value;
 
         if (senha.length === 0) {
---
+++ PHP/cadastro.php
@@ -0,0 +1,9 @@
+<?php
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $senha = $_POST['senha'];
+
+?>
+
